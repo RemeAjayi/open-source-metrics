81b542c5b9e19ae666878e0f4e0c0d3e5ca89c36
Reme <remeajayi@gmail.com>
2024-05-15 19:11:09 -0400

    Set up Kafka

--- src/main/java/com.reme/GithubAPIController.java
+++ src/main/java/com.reme/GithubAPIController.java
@@ -1,18 +1,30 @@
 package com.reme;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class GithubAPIController {
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    public GithubAPIController(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
     @GetMapping("/prs")
     public ResponseEntity<String> index() {
         RestTemplate restTemplate = new RestTemplate();
         String url = "https://api.github.com/repos/apache/spark/pulls?since=2024-05-02T00:00:00Z";
 //        TODO:Parameterize the since date
         return restTemplate.getForEntity(url, String.class);
+    }
+
+    @GetMapping("/publish")
+    public void publish(@RequestParam String message) {
+// change to requestbody annotation
+        kafkaTemplate.send("open-source-pull-requests", message);
     }
 }
 
---
+++ src/main/java/com.reme/model/PullRequest.java
@@ -0,0 +1,7 @@
+package com.reme.model;
+
+public class PullRequest {
+
+}
+
+
